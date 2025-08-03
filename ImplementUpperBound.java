1e59a0c73c637c1b3ce7a356f2d1a1661a3d0279
DeepakGahlawat <deepakgahlawat000@gmail.com>
2025-08-03 07:56:59 +0530

    ImplementUpperBound

---
+++ ImplementUpperBound.java
@@ -0,0 +1,51 @@
+// Given a sorted array arr[] and a number target, the task is to find the upper bound of the target in this given array.
+// The upper bound of a number is defined as the smallest index in the sorted array where the element is greater than the given number.
+
+// Note: If all the elements in the given array are smaller than or equal to the target, the upper bound will be the length of the array.
+
+// Examples :
+
+// Input: arr[] = [2, 3, 7, 10, 11, 11, 25], target = 9
+// Output: 3
+// Explanation: 3 is the smallest index in arr[], at which element (arr[3] = 10) is larger than 9.
+// Input: arr[] = [2, 3, 7, 10, 11, 11, 25], target = 11
+// Output: 6
+// Explanation: 6 is the smallest index in arr[], at which element (arr[6] = 25) is larger than 11.
+// Input: arr[] = [2, 3, 7, 10, 11, 11, 25], target = 100
+// Output: 7
+// Explanation: As no element in arr[] is greater than 100, return the length of array.
+// Constraints:
+// 1 ≤ arr.size() ≤ 106
+// 1 ≤ arr[i] ≤ 106
+// 1 ≤ target ≤ 106
+
+
+class ImplementUpperBound {
+    int upperBound(int[] arr, int target) {
+        int n = arr.length;
+        int ans = n;
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        ImplementUpperBound sol = new ImplementUpperBound();
+        int[] arr = {1, 2, 4, 4, 5, 7, 9}; // Sample input
+        int target = 4;
+
+        int result = sol.upperBound(arr, target);
+        System.out.println("Upper bound index: " + result);
+    }
+}
+
